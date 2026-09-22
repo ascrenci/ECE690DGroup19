78d8ce0dc6c920072dbe435becd182d03d0230af
ascrenci <ascrenci@umass.edu>
2026-09-22 13:03:14 -0400

    testing github actions

---
+++ ContentView.swift
@@ -0,0 +1,13 @@
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Text("Hello, iPhone!")
+                .font(.largeTitle)
+
+            Text("Built on GitHub Actions")
+                .padding()
+        }
+    }
+}
